9fb77e9d7473d3320bd3d1dc85f2f5a53c592d8c
kkonteh97 <55326260+kkonteh97@users.noreply.github.com>
2024-05-17 20:55:28 -0700

    Initial Commit

---
+++ SimpleAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleAssistant
+//
+//  Created by kemo konteh on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleAssistant/SimpleAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAssistantApp.swift
+//  SimpleAssistant
+//
+//  Created by kemo konteh on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
